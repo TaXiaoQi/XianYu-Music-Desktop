95ada734e0b0a5dea089a396d5155343db30c0dd
lvzin <han.gang@lvzin.com>
2026-07-27 20:50:32 +0800

    解决了底栏在播放页和非播放页共用一个”取消固定“按钮的问题

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,3 +5,8 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+
+declare module 'blueimp-md5' {
+  const md5: (input: string) => string;
+  export default md5;
+}
